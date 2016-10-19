1d65fd68050a9929571a68f3188afc5a4c4f9129
jisooyun <jisooyun1994@gmail.com>
2016-10-19 15:04:18 +0200

    jquery mobile

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-  function calc() {
-        var bill = Number(document.getElementById('bill').value);
-        var tip = bill * .15;
-        var total_bill = bill + tip;
+function calc() {
+	var prix = $('#prix').val();
+        var tip = prix * .15;
+        var prix_total = prix + tip;
 
-        document.getElementById("tip").innerHTML= "$"+Number(tip).toFixed(2);
-        document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);         
+        document.getElementById("tip").innerHTML= "€"+Number(tip).toFixed(2);
+        document.getElementById("total").innerHTML= "€"+Number(prix_total).toFixed(2);         
     }
